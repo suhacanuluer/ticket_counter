ab7508a28d29fca97a6fd067078b1e2df335b3af
Suha Can Uluer <sculuer@gmail.com>
2022-10-21 17:03:31 +0300

    Initial Commit

---
+++ Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by Süha Can Uluer on 21.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
