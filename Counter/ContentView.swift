8e3cfd4707ff39d1f9b5c0374ba08025b66489c9
Suha Can Uluer <sculuer@gmail.com>
2022-11-01 00:30:43 +0300

    added new tickets

--- Counter/ContentView.swift
+++ Counter/ContentView.swift
@@ -10,18 +10,37 @@ import SwiftUI
 struct ContentView: View {
     @ObservedObject var entryTicket = TicketModel("Giriş", 20)
     @ObservedObject var entryWithAlcoholTicket = TicketModel("Alkollu Giriş", 50)
+    @ObservedObject var entryWithDinnerTicket = TicketModel("Yemekli Giriş", 100)
+    @ObservedObject var entryWithChildTicket = TicketModel("Çocuklu Giriş", 70)
+    @ObservedObject var entryWithSpeacialTicket = TicketModel("Özel Giriş", 200)
+    
     var body: some View {
-        VStack {
+        let totalAllPrice = entryTicket.totalTicketPrice +
+                            entryWithAlcoholTicket.totalTicketPrice +
+                            entryWithDinnerTicket.totalTicketPrice +
+                            entryWithChildTicket.totalTicketPrice +
+                            entryWithSpeacialTicket.totalTicketPrice
+        ScrollView {
             VStack {
-                TicketView(ticketModel: entryTicket)
-                Divider().background(.red)
-                TicketView(ticketModel: entryWithAlcoholTicket)
-                Divider().background(.red)
-                Text("Toplam Para: \(entryTicket.totalTicketPrice + entryWithAlcoholTicket.totalTicketPrice)")
+                VStack {
+                    Group {
+                        TicketView(ticketModel: entryTicket)
+                        Divider().background(.red)
+                        TicketView(ticketModel: entryWithAlcoholTicket)
+                        Divider().background(.red)
+                        TicketView(ticketModel: entryWithDinnerTicket)
+                        Divider().background(.red)
+                        TicketView(ticketModel: entryWithChildTicket)
+                        Divider().background(.red)
+                        TicketView(ticketModel: entryWithSpeacialTicket)
+                        Divider().background(.red)
+                    }
+                    Text("Toplam Para: \(totalAllPrice)")
+                }
+                .font(.system(size: 25))
             }
-            .font(.system(size: 25))
+            .padding()
         }
-        .padding()
     }
 }
 
